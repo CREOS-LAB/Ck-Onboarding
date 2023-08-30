0fc180625abf99a1abb9c6c31eafb50dc427e0c4
DevEmmy <eolaosebikan60@gmail.com>
2023-08-30 18:05:08 +0100

    updated cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,9 @@ const whitelist: String[] = [
     'http://localhost:3000', 
     'http://127.0.0.0.1:3000', 
     'https://your-site.com',
-    "*"
+    "*",
+    "https://curiouz-kids.vercel.app",
+    "https://ck-kids-dashboard.vercel.app/"
         ]
                 
     const corsOptions = {
