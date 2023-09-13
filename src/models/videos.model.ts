b5837a5da71ca7f875ceb5172c453cd0bea9e4d9
DevEmmy <eolaosebikan60@gmail.com>
2023-09-13 18:21:27 +0100

    edit field

--- src/models/videos.model.ts
+++ src/models/videos.model.ts
@@ -3,7 +3,7 @@ import mongoose, {Schema, Types} from "mongoose"
 const schema = new Schema({
     name: {type: String, required: true},
     link: {type: String, required: true},
-    collectionRelation: {type: Schema.Types.ObjectId, ref: "Collection", required: false},
+    collectionRelation: {type: Schema.Types.ObjectId, ref: "Collections", required: false},
     views: {type: Number, default: 0},
     watched: {type: Number, default: 0},
     cover: {type: String, required: true}
